5ff134aa59c4ebc49b477eab1c7f35ff4a2773e2
AgolaDeveloper <owinoagola00@gmail.com>
2021-03-27 22:58:04 +0300

    changiing Home

--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,7 @@ function Home(){
     return(
     <div className="cont">
         <img src={chess} width="200px" alt="ghettochess" />
-        <h1>WELCOME TO <emph>GHETTO CHESS</emph></h1>
+        <h1>WELCOME TO OUR<emph>GHETTO CHESS</emph></h1>
         
         <p>At ghetto chess, we flex our brain muscles and all are welcome <br/>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quo quisquam vel saepe assumenda corrupti provident dicta voluptatum id eos, recusandae dolorem, iure ea repellat aut voluptate ipsum ratione modi molestiae ullam iusto incidunt debitis temporibus. Porro aliquam quae hic!
